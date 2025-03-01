166a4d55ed09dc47f73d044e0cc3d0536e8ca576
aleksanteri <aleksanteri.lybeck@outlook.com>
2025-03-01 14:10:17 +0200

    Uusimmat versiot Gittiin

--- public/script.js
+++ public/script.js
@@ -1,3 +1,25 @@
+let BOARD_SIZE = 15
+let board;
+
 function startGame(){
     console.log("peli aloitettu")
+
+    document.getElementById('intro-screen').style.display = 'none';
+    document.getElementById('game-screen').style.display = 'block';
+    board = generateRandomBoard();
+    console.log(board);
+}
+
+function generateRandomBoard() {
+    const newBoard = Array.from({ length: BOARD_SIZE }, () => Array(BOARD_SIZE).fill(' '));
+
+    for (let y = 0; y < BOARD_SIZE; y++) {
+        for (let x = 0; x < BOARD_SIZE; x++) {
+            if (y === 0 || y === BOARD_SIZE - 1 || x === 0 || x === BOARD_SIZE - 1) {
+                newBoard[y][x] = 'W';
+            }
+        }
+    }
+
+    return newBoard;
 }
